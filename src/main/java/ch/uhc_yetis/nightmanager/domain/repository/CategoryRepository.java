67602d53ccc96e7104437db61babde3be1740894
lasher23 <niciraffi@gmail.com>
2018-08-28 18:37:25 +0200

    delete old project structure

--- src/main/java/ch/uhc_yetis/nightmanager/domain/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package ch.uhc_yetis.nightmanager.domain.repository;
-
-import ch.uhc_yetis.nightmanager.domain.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
