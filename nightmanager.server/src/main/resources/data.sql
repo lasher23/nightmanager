e41aae1a3fbe7154ff6eccb7063a161b12df9112
Nicolas Schmid <Nicolas_Schmid@sluz.ch>
2018-11-22 09:12:00 +0100

    change data.sql to execute csv files

--- nightmanager.server/src/main/resources/data.sql
+++ nightmanager.server/src/main/resources/data.sql
@@ -1,11 +1,11 @@
---copy halls(id,name)
---From 'C:\projects\nightmanager\nightmanager.server\src\main\resources\Hall.csv' DELIMITER ',' CSV HEADER;
+copy halls(id,name)
+From './Hall.csv' DELIMITER ',' CSV HEADER;
 
---copy categories(id,name,state,type)
---From 'C:\projects\nightmanager\nightmanager.server\src\main\resources\Kategorie.csv' DELIMITER ',' CSV HEADER;
+copy categories(id,name,state,type)
+From './Kategorie.csv' DELIMITER ',' CSV HEADER;
 
---copy teams(id,name,flag_placeholder,fk_category,rank)
---From 'C:\projects\nightmanager\nightmanager.server\src\main\resources\Team.csv' DELIMITER ',' CSV HEADER;;
+copy teams(id,name,flag_placeholder,fk_category,rank)
+From './Team.csv' DELIMITER ',' CSV HEADER;;
 
---copy games(ID,fk_team_home,fk_team_guest,goals_team_home,goals_team_guest,state,fk_hall,fk_category,start_date,flag_placeholder,type,duration)
---From 'C:\projects\nightmanager\nightmanager.server\src\main\resources\Match.csv' DELIMITER ',' CSV HEADER;;
+copy games(ID,fk_team_home,fk_team_guest,goals_team_home,goals_team_guest,state,fk_hall,fk_category,start_date,flag_placeholder,type,duration)
+From './Match.csv' DELIMITER ',' CSV HEADER;;
